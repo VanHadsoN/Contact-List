aa7fe23c11b5bd60ae378f47e806488458271771
vanhadson <vanhadson@gmail.com>
2025-07-27 10:35:20 +0300

    changed src/index.ts (added const nameInput, const vacancyInput, const phoneInput, const addButton and addButton.addEventListener)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
 import { contacts, addContact, groupByLetter, Contact } from './contacts/index';
 
+const nameInput = document.querySelector<HTMLInputElement>('#name')!;
+const vacancyInput = document.querySelector<HTMLInputElement>('#vacancy')!;
+const phoneInput = document.querySelector<HTMLInputElement>('#phone')!;
+const addButton = document.querySelector<HTMLButtonElement>('#add-button')!;
+
+addButton.addEventListener('click', () => {
+    const name = nameInput.value.trim();
+    const vacancy = vacancyInput.value.trim();
+    const phone = phoneInput.value.trim();
+
+    // валидация
+    if (!name || !vacancy || !phone) {
+        console.warn('All fields are required');
+        return;
+    }
+
+    addContact( { name, vacancy, phone } );
+
+    console.log('Current contact list:', contacts);
+
+    nameInput.value = '';
+    vacancyInput.value = '';
+    phoneInput.value = '';
+});
