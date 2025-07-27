84d25fcb9d6c57c911dc53bdab8ab83345334a67
vanhadson <vanhadson@gmail.com>
2025-07-27 12:52:58 +0300

    created webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.ts', // точка входа
+	devtool: 'inline-source-map', // для отладки
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/, // обработка файлов .ts и .tsx
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/, // обработка CSS
+				use: ['style-loader', 'css-loader']
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js', // итоговый бандл
+		path: path.resolve(__dirname, 'dist'), // директория для сборки
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		})
+	],
+	devServer: {
+		static: './dist', // директория для dev-сервера
+		port: 3000 // порт
+	}
+};
